d492dad21ddb56f51ec396a4b1c238847acdb432
Evan Suma Rosenberg <suma@umn.edu>
2024-02-29 11:39:11 -0600

    pre-class commit

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-/* GopherGfx App Template
+/* Lecture 14: Mesh Morphing
  * CSCI 4611, Spring 2024, University of Minnesota
  * Instructor: Evan Suma Rosenberg <suma@umn.edu>
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
@@ -8,24 +8,49 @@ import * as gfx from 'gophergfx'
 
 export class App extends gfx.GfxApp
 {
+    private cameraControls: gfx.OrbitControls;
+
     // --- Create the App class ---
     constructor()
     {
         // initialize the base class gfx.GfxApp
         super();
+
+        this.cameraControls = new gfx.OrbitControls(this.camera);
     }
 
 
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        // Setup camera
+        this.camera.setPerspectiveCamera(60, 1920/1080, .1, 20)
+        this.cameraControls.setTargetPoint(new gfx.Vector3(0, 1, 0));
+        this.cameraControls.setDistance(3);
 
+        // Set a black background
+        this.renderer.background.set(0, 0, 0);
+        
+        // Create an ambient light
+        const ambientLight = new gfx.AmbientLight(new gfx.Color(0.25, 0.25, 0.25));
+        this.scene.add(ambientLight);
+
+        // Create a directional light
+        const pointLight = new gfx.PointLight(new gfx.Color(1.25, 1.25, 1.25));
+        pointLight.position.set(2, 1, 3)
+        this.scene.add(pointLight);
+
+        // Create the ground
+        const ground = gfx.Geometry3Factory.createBox(5, 1, 5);
+        ground.material.setColor(new gfx.Color(0, 0.5, 0.5));
+        ground.position.y = -0.5;
+        this.scene.add(ground);
     }
 
     
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
-
+        this.cameraControls.update(deltaTime);
     }
 }
